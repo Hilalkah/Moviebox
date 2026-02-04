0becd00f70f59f95219e45dd6bd149479b3527f8
Hilal Kahraman <hllkhrmn@hotmail.com>
2026-02-04 13:01:12 +0300

    Initial Commit

---
+++ Moviebox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moviebox
+//
+//  Created by Hilal Kahraman on 4.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
