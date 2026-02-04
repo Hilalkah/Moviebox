053d4235e729196e98a79a8f94bf7eedb42122db
Hilal Kahraman <hllkhrmn@hotmail.com>
2026-02-04 13:40:31 +0300

    Add MovieList for route

---
+++ Moviebox/Scenes/MovieList/MovieListViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MovieListViewController.swift
+//  Moviebox
+//
+//  Created by Hilal Kahraman on 4.02.2026.
+//
+
+import UIKit
+
+final class MovieListViewController: UIViewController {
+    
+}
